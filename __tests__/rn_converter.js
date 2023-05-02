39bbc5a35abc09326c0c2210a7b2350e627e720a
reecem02 <reecehmorgan@gmail.com>
2023-05-01 21:11:03 -0700

    changed parameters to only take inputs btw 1-3999

--- __tests__/rn_converter.js
+++ __tests__/rn_converter.js
@@ -6,11 +6,6 @@ test("is a valid roman numeral under 5 test #1", function (){
     expect(result).toBe("III")
 })
 
-test("is a valid roman numeral under 5 test #2", function (){
-    const rn = "0"
-    const result = rn_converter(rn)
-    expect(result).toBe("")
-})
 
 test("tests number 5", function (){
     const rn = "5"
