c79c0c347a8558007df60e3555ac614eb12a2f6d
reecem02 <reecehmorgan@gmail.com>
2023-05-01 19:48:59 -0700

    added more test cases and started testing "5"

--- __tests__/rn_converter.js
+++ __tests__/rn_converter.js
@@ -1,7 +1,19 @@
 const rn_converter = require("../rn_converter.js")
 
-test("is a valid roman numeral", function (){
+test("is a valid roman numeral under 5 test #1", function (){
     const rn = "3"
     const result = rn_converter(rn)
     expect(result).toBe("III")
+})
+
+test("is a valid roman numeral under 5 test #2", function (){
+    const rn = "0"
+    const result = rn_converter(rn)
+    expect(result).toBe("")
+})
+
+test("tests number 5", function (){
+    const rn = "5"
+    const result = rn_converter(rn)
+    expect(result).toBe("V")
 })
